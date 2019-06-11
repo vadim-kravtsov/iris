5b3e5f135073ae17f49ca83dcfe81798b8a1aa48
Vadim <mr.vadim.kravtsov@mail.ru>
2019-06-11 20:53:55 +0300

    Create main window

--- libs/MainApplication.py
+++ libs/MainApplication.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
 
+import tkinter as Tk
+from tkinter import messagebox
+
 
 class MainApplication(object):
     def __init__(self):
-        pass
+        # Create root window
+        # Create main window base frame
+        self.root = Tk.Tk()
+
+        # Configure root window
+        self.root.title("IRIS")
+        self.root.protocol('WM_DELETE_WINDOW', self.shutdown)
+        self.root.geometry("800x600+100+100")
+        #self.root.resizable(0, 0)
+
+        self.root.mainloop()
+
+    def shutdown(self):
+        """
+        This function is called when the user hit close window button
+        """
+        answer = messagebox.askokcancel("Close", "Really close?")
+        if answer is True:
+            self.alive = False
+            # Close the main window
+            self.root.destroy()
