d2d327761e318cf77d0305b570321c2ff4e4bc6b
Vadim <mr.vadim.kravtsov@mail.ru>
2019-06-14 17:52:16 +0300

    Added search line and implemented regular expressions search

--- libs/filesystem.py
+++ libs/filesystem.py
@@ -1,7 +1,12 @@
 from pathlib import Path
+from PyQt5.QtGui import QStandardItem
+from PyQt5 import QtCore
 
 
 class ListOfFiles(object):
     def __init__(self, dir_path):
         self.file_paths = list(Path(dir_path).glob('*.*'))
         self.file_names = [p.parts[-1] for p in self.file_paths]
+        self.qt_filelist = [QStandardItem(x) for x in self.file_names]
+        for item in self.qt_filelist:
+            item.setData(self.file_paths[self.qt_filelist.index(item)], role=QtCore.Qt.UserRole)
--- libs/gui/WorkDirBrowser.py
+++ libs/gui/WorkDirBrowser.py
@@ -1,10 +1,11 @@
 from ..graphics import PlotCanvas
+from fnmatch import fnmatch
 from PyQt5 import QtCore, QtWidgets, QtGui
 
 
 class WorkDirBrowser(QtWidgets.QWidget):
     '''Window in which user can mark files as bias, dark, flat, etc.'''
-    def __init__(self, parent, ListOfFiles):
+    def __init__(self, parent, listOfFiles):
         super().__init__(parent=parent)
 
         # Set window properties
@@ -13,37 +14,61 @@ class WorkDirBrowser(QtWidgets.QWidget):
 
         # Create containers
         self.hbox = QtWidgets.QHBoxLayout()
-        self.vbox = QtWidgets.QVBoxLayout()
+        self.vbox_left = QtWidgets.QVBoxLayout()
+        self.vbox_right = QtWidgets.QVBoxLayout()
 
         # Create and fill model
         self.list_model = QtGui.QStandardItemModel()
-        self.list_model.appendColumn([QtGui.QStandardItem(x) for x in ListOfFiles.file_names])
+        self.list_model.appendColumn(listOfFiles.qt_filelist)
 
-        # Addind paths in model as data with role=UserRole
-        i = 0
-        for path in ListOfFiles.file_paths:
-            self.list_model.setData(self.list_model.index(i, 0), path, role=QtCore.Qt.UserRole)
-            i += 1
-
-        # Customizing widgets
+        # Create files list widgets
         self.file_list = QtWidgets.QListView()
         self.file_list.setModel(self.list_model)  # Set model as list_model
         self.file_list.setWrapping(False)  # No line breaks
         self.file_list.setSelectionMode(3)  # Set extended selection
         self.file_list.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)  # Disable editing
 
+        # Create title
+        self.file_list_label = QtWidgets.QLabel("Untagged files:", parent=self)
+        self.file_list_label.setAlignment(QtCore.Qt.AlignCenter)
+
+        # Create search line
+        self.file_list_text_field = QtWidgets.QLineEdit(parent=self)
+        self.file_list_text_field.setCompleter(QtWidgets.QCompleter(listOfFiles.file_names, parent=self))
+        self.file_list_text_field.setPlaceholderText("Enter file mask...")
+
+        def search_by_mask(self, mask):
+            """Set as visible only file names which match the mask"""
+            file_names = listOfFiles.file_names
+            if mask:
+                indxs = [file_names.index(x) for x in file_names if fnmatch(x, mask)]
+                for i in range(len(file_names)):
+                    if i in indxs:
+                        self.file_list.setRowHidden(i, False)
+                    else:
+                        self.file_list.setRowHidden(i, True)
+            else:
+                for i in range(len(file_names)):
+                    self.file_list.setRowHidden(i, False)
+
+        # Showing fits file, choosed by mouseclick or by pressing "Return"
+        self.file_list.activated.connect(lambda: self.plot_canvas.plot(self.file_list.selectedIndexes()))
+
+        # Hiding file names as don't match the mask
+        self.file_list_text_field.textChanged.connect(lambda: search_by_mask(self, self.file_list_text_field.text()))
+
         self.plot_canvas = PlotCanvas()
         self.file_list3 = QtWidgets.QListView()
-
-        # Associating choosing element from file_list with plot function
-        self.file_list.activated.connect(lambda: self.plot_canvas.plot(self.file_list.selectedIndexes()))
 
         self.file_list3.setModel(self.list_model)
 
         # Packaging widgets in containers
-        self.hbox.addWidget(self.file_list)
-        self.vbox.addWidget(self.plot_canvas)
-        self.vbox.addWidget(self.file_list3)
-        self.hbox.addLayout(self.vbox)
+        self.vbox_left.addWidget(self.file_list_label)
+        self.vbox_left.addWidget(self.file_list_text_field)
+        self.vbox_left.addWidget(self.file_list)
+        self.hbox.addLayout(self.vbox_left)
+        self.vbox_right.addWidget(self.plot_canvas)
+        self.vbox_right.addWidget(self.file_list3)
+        self.hbox.addLayout(self.vbox_right)
         self.setLayout(self.hbox)
         self.show()
